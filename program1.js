b011e7865ef24af8aad68f3e0e7da5c0f4c40d98
rajeshwarne <rajeshwarne1@gmail.com>
2024-11-11 15:34:12 +0530

    program 1 and program 2 completed

--- program1.js
+++ program1.js
@@ -3,7 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const bracketMap = { ')': '(', '}': '{', ']': '[' };
+    const stack = [];
     
+    for (let char of s) {
+        if (char in bracketMap) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
+    let total = 0;
+    let prevValue = 0;
     
+    for (let i = s.length - 1; i >= 0; i--) {
+        const value = romanMap[s[i]];
+        if (value < prevValue) {
+            total -= value;
+        } else {
+            total += value;
+        }
+        prevValue = value;
+    }
+    
+    return total;
 };
 
 
